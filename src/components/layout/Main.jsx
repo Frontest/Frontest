23cc1eee796f377d23fcc97d6fabd915868231ee
Soroosh Jaberi <93154421+sorooshjaberi@users.noreply.github.com>
2023-06-29 18:57:48 +0430

    main part's header

--- src/components/layout/Main.jsx
+++ src/components/layout/Main.jsx
@@ -1,6 +1,39 @@
+import { RiUserLine } from "react-icons/ri";
+import { SlArrowDown } from "react-icons/sl";
+import { CiMail } from "react-icons/ci";
+
 const Main = () => {
   return (
-    <main className="flex-1 bg-white">Main</main>
-  )
-}
-export default Main
+    <main className="flex-1 bg-white">
+      <div className="flex h-[80px] w-full items-center justify-between px-[42px] shadow-lg">
+        <div className=" user-info flex items-center">
+          <div className="ml-[10px] flex h-[40px] w-[40px] items-center justify-center rounded-full bg-[#f1f1f1]">
+            <RiUserLine className="text-[24px]" />
+          </div>
+          <div className="felx-col  justify-between text-[15px]">
+            <div>کیمیا جوکار</div>
+            <div className="flex items-center text-[--blue-primary]">
+              <span className=" text-[13px]">فنی - نرم افزار</span>
+              <SlArrowDown className="mr-[5px] text-[10px]" />
+            </div>
+          </div>
+        </div>
+        <div className="user-stats flex h-[40px] w-[240px] items-center  ">
+          <div className="reports text-[14px]">
+            <span className="label ml-[10px] text-[--blue-primary]">
+              تعداد گزارشات:
+            </span>
+            <span className="content">
+              {Number(107752).toLocaleString().latin2Persian()}
+            </span>
+          </div>
+          <div className="mx-[15px] w-[2px] self-stretch bg-[--blue-primary]">
+            &nbsp;
+          </div>
+          <CiMail className="text-[20px]" />
+        </div>
+      </div>
+    </main>
+  );
+};
+export default Main;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,29 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+String.prototype.latin2Persian = function () {
+  const latinNumbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+  const persianNumbers = ["۰", "۱", "۲", "۳", "۴", "۵", "۶", "۷", "۸", "۹"];
+  let result = "";
+
+  for (let i = 0; i < this.length; i++) {
+    const char = this.charAt(i);
+    const index = latinNumbers.indexOf(char);
+
+    if (index !== -1) {
+      result += persianNumbers[index];
+    } else {
+      result += char;
+    }
+  }
+
+  return result;
+};
+
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
